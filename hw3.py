ecdb565dd2ccbe25c252cce0a009ed48a676893e
facedesk <dgold@sjnma.org>
2018-09-24 07:49:10 -0500

    Create hw3.py

---
+++ hw3.py
@@ -0,0 +1,23 @@
+# The goal of this homework assignment is to flex your lists, strings, and string manipulation skills we are learning in class
+# Avoid writing any loops (aside from the one I have provided for you).
+
+print('''
+Welcome to Saint John's Northwestern Military Academy!
+This is your Academic Planner Program that will help you keep your assignments in line
+''')
+
+homework_to_do=[]
+homework_due_dates=[]
+
+tests_quizzes=[]
+tests_quiz_dates=[]
+
+while(True):
+    '''
+    problem 1)
+    '''
+    input("?")
+
+
+
+
