132ad858c09cc7e1bfc713c7b189140492980a49
David Gold <dgold@SJNMA.ORG>
2018-09-25 07:39:33 -0500

    add lists.py with pyplot example

---
+++ practice_code/lists.py
@@ -0,0 +1,34 @@
+#import matplotlib.pyplot as plt
+
+
+
+x1=0
+x2=1
+x3=2
+
+y1=1
+y2=2
+y3=4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x=[1,2,3,4,5]
+y=[1,4,9,16,25]
+
+plt.title("y=x**2")
+plt.plot(x,y)
+plt.show()
+
+input("press enter")
+
