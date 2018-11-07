8a88357bc37a54205170e61031171618b078f2fe
David Gold <dgold@SJNMA.ORG>
2018-11-07 07:17:23 -0600

    add changes to recursion to make full lesson plan

--- practice_code/recursion.py
+++ practice_code/recursion.py
@@ -22,3 +22,44 @@ def countdown(x=10):
     else:
         return countdown(x-1)
 countdown()
+
+
+'''
+We have bunnies standing in a line, 
+numbered 1, 2, ... The odd bunnies (1, 3, ..)
+ have the normal 2 ears. The even bunnies 
+ (2, 4, ..) we'll say have 3 ears, because 
+ they each have a raised foot. Recursively 
+ return the number of "ears" in the 
+ bunny line 1, 2, ... n (without loops or 
+ multiplication).
+'''
+def simple_bunny_ears(bunnies):
+    if(bunnies == 0):
+        return 0
+    else:
+        return 2 + simple_bunny_ears(bunnies -1)    
+
+
+def odd_bunny_ears(bunnies):
+    if(bunnies == 0):
+        return 0
+    elif(bunnies %2 == 0):
+        return 2 + odd_bunny_ears(bunnies -1)
+    else:
+        return 1 + odd_bunny_ears(bunnies -1)
+
+
+'''
+towers of hanoi
+'''
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+
+def moveDisk(fp,tp):
+    print("moving disk from",fp,"to",tp)
+
+moveTower(3,"A","B","C")
